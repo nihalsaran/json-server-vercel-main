0a6a025258b8b2dc7ce257e3e1598266054131d9
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-05-28 20:39:41 +0530

    changes

--- api/server.js
+++ api/server.js
@@ -1,36 +1,29 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-const fs = require('fs')
-const path = require('path')
 
 const server = jsonServer.create()
+
+// Uncomment to allow write operations
+const fs = require('fs')
+const path = require('path')
 const filePath = path.join('db.json')
-const data = fs.readFileSync(filePath, "utf-8")
-const db = JSON.parse(data)
-const router = jsonServer.router(db)
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+// const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+const router = jsonServer.router('db.json')
+
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+// Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/api/*': '/$1?_save=true',
+    '/blog/:resource/:id/show': '/:resource/:id'
 }))
-
-// Middleware to save changes to db.json
-server.use((req, res, next) => {
-    if (req.method === 'POST' || req.method === 'PUT' || req.method === 'PATCH' || req.method === 'DELETE') {
-        try {
-            fs.writeFileSync(filePath, JSON.stringify(router.db.getState(), null, 2));
-        } catch (error) {
-            console.error('Error saving changes to db.json:', error);
-            return res.status(500).send('Error saving changes to db.json');
-        }
-    }
-    next();
-});
-
 server.use(router)
-server.listen(3001, () => {
+server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
